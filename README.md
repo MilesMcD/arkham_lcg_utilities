adb8b9a20aa58f569e38b4c3c72c09e11b423b37
MilesMcD <k14mm02@gmail.com>
2022-08-16 23:12:06 -0500

    Project backbone

---
+++ README.md
@@ -0,0 +1,6 @@
+ARKHAM HORROR LCG UTILITIES
+
+TODO:
+Import Card set
+Import decklists for 1 character from period of 1 mo
+35q bre
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Arkhamhorrorutilities',
+      version='1.0',
+      description='Deckbuilding utilities for the Arkham Horror LCG',
+      author='Miles McDowall, Owen Galvin',
+      author_email='___',
+      packages=['arkhamhorrorutilities'],
+      install_requires=[]
+     )
