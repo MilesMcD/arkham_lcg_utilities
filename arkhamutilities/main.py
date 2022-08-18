1f0a277f96640f52b2cae4e0eadbb08b0c31a4e3
MilesMcD <k14mm02@gmail.com>
2022-08-17 22:52:58 -0500

    minor changes to main.py adding return statements

--- arkhamutilities/main.py
+++ arkhamutilities/main.py
@@ -2,13 +2,10 @@ import datetime
 import pandas as pd
 import requests
 import db
+import json
 
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Grabs decks from a given date and returns them as a dictionary
 def request_decklists_by_date(date: datetime.date):
     url = "https://arkhamdb.com/api/public/decklists/by_date/{0}-{1}-{2}".format(
         date.year,
@@ -19,16 +16,17 @@ def request_decklists_by_date(date: datetime.date):
     response = requests.get(url)
     print(response)
     print(response.json())
+    return response.json()
 
-
-# makes a request to https://arkhamdb.com/api/public/decklists/by_date/ for each date in the range
+# Makes a request to https://arkhamdb.com/api/public/decklists/by_date/ for each date in the range
 def request_decklists_by_date_range(from_date: datetime.date, to_date: datetime.date):
+    all_decklists = []
     for single_date in pd.date_range(from_date, to_date):  # TODO replace code if don't want to use pandas elsewhere
         print(single_date)
-        # request_decklists_by_date(single_date)
-
+        all_decklists.add(request_decklists_by_date(single_date))
+    return all_decklists
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     db.initialize()
-    request_decklists_by_date_range(datetime.datetime(2022, 8, 1), datetime.date.today())
+    request_decklists_by_date_range(datetime.datetime(2022, 8, 1), datetime.datetime(2022, 8, 5)) #datetime.date.today())
